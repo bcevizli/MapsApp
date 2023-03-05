d4ccda11343de638cec041b3de541220ad515624
burak cevizli <burak.cevizli7@gmail.com>
2023-03-05 20:39:34 +0300

    View Controller and Extensions

---
+++ MapsApp/Extensions/CLLocation+Extentions.swift
@@ -0,0 +1,16 @@
+//
+//  CLLocation+Extentions.swift
+//  MapsApp
+//
+//  Created by Adem Burak Cevizli on 5.03.2023.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocation {
+    static var `default`: CLLocation {
+        CLLocation(latitude: 36.063457, longitude: -95.880516)
+    }
+}
+
